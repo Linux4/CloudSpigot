840d326dc7f847cc8d56c96879bc4a58156d5702
Server24-7 <tim@server24-7.eu>
2018-09-10 19:37:14 +0200

    removed work/

--- work/1.8.9/net/minecraft/server/InventoryHorseChest.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.server;
-
-public class InventoryHorseChest extends InventorySubcontainer {
-
-    public InventoryHorseChest(String s, int i) {
-        super(s, false, i);
-    }
-}
