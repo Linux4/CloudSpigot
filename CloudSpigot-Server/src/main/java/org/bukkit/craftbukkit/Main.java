2936bb6557fda3fb0b68d33897025edf233c21b8
Server24-7 <tim@server24-7.eu>
2018-09-13 16:32:03 +0200

    fixed mysql driver not working & updated mysql driver

--- CloudSpigot-Server/src/main/java/org/bukkit/craftbukkit/Main.java
+++ CloudSpigot-Server/src/main/java/org/bukkit/craftbukkit/Main.java
@@ -17,6 +17,11 @@ public class Main {
     public static boolean useConsole = true;
 
     public static void main(String[] args) {
+        try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch(ClassNotFoundException ex) {
+                ex.printStackTrace();
+        }
         // Todo: Installation script
         OptionParser parser = new OptionParser() {
             {
