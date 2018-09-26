746bca25f38903e91afb1c02b4126893501cfdbe
Server24-7 <tim@server24-7.eu>
2018-09-26 17:49:43 +0200

    Faster redstone torch rapid clock removal

--- src/main/java/net/minecraft/server/BlockRedstoneTorch.java
+++ src/main/java/net/minecraft/server/BlockRedstoneTorch.java
@@ -116,9 +116,17 @@ public class BlockRedstoneTorch extends BlockTorch {
         boolean flag = this.g(world, blockposition, iblockdata);
         List list = (List) BlockRedstoneTorch.b.get(world);
 
-        while (list != null && !list.isEmpty() && world.getTime() - ((BlockRedstoneTorch.RedstoneUpdateInfo) list.get(0)).b > 60L) {
-            list.remove(0);
-        }
+        // CloudSpigot start
+        if (list != null) {
+            int index = 0;
+            while (index < list.size() && world.getTime() - ((BlockRedstoneTorch.RedstoneUpdateInfo) list.get(index)).getTime() > 60L) {
+                index++;
+            }
+            if (index > 0) {
+                list.subList(0, index).clear();
+            }
+         }
+        // CloudSpigot end
 
         // CraftBukkit start
         org.bukkit.plugin.PluginManager manager = world.getServer().getPluginManager();
@@ -197,7 +205,7 @@ public class BlockRedstoneTorch extends BlockTorch {
     static class RedstoneUpdateInfo {
 
         BlockPosition a;
-        long b;
+        long b; long getTime() { return this.b; } // CloudSpigot - OBFHELPER
 
         public RedstoneUpdateInfo(BlockPosition blockposition, long i) {
             this.a = blockposition;
