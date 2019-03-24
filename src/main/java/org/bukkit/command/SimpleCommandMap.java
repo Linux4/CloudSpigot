04a9f9f7f5683bf0aa4ec4d26bbe324e479ac14f
Server24-7 <tim@server24-7.eu>
2019-03-24 17:41:52 +0100

    add /cloudspigot

--- src/main/java/org/bukkit/command/SimpleCommandMap.java
+++ src/main/java/org/bukkit/command/SimpleCommandMap.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
 import org.bukkit.Server;
+import org.bukkit.command.defaults.CloudSpigotCommand;
 import org.bukkit.command.defaults.HelpCommand;
 import org.bukkit.command.defaults.PluginsCommand;
 import org.bukkit.command.defaults.ReloadCommand;
